8f674ec1202e729e6fa7bc689aed42cd1ba52c82
wabualela <wailabualela@gmail.com>
2024-03-11 07:42:00 +0300

    Add user create fun

--- classes/api.php
+++ classes/api.php
@@ -57,7 +57,7 @@ class api {
      *
      * @return bool
      */
-    public function is_enabled() {
+    public static function is_enabled() {
         return is_enabled_auth('twilio');
     }
 
@@ -117,10 +117,65 @@ class api {
 
     public function get_countries_choices() {
         $countries = get_string_manager()->get_list_of_countries();
-        $choices = [];
+        $choices   = [];
         foreach ($countries as $key => $value) {
             $choices[] = $value;
         }
         return $choices;
+    }
+
+    /**
+     * Create an account with a linked login that is already confirmed.
+     *
+     * @param array $userinfo as returned from an oauth client.
+     * @return bool
+     */
+    public static function create_new_confirmed_account($userinfo) {
+        global $CFG, $DB;
+        require_once($CFG->dirroot . '/user/profile/lib.php');
+        require_once($CFG->dirroot . '/user/lib.php');
+
+        $user             = new stdClass();
+        $user->auth       = 'twilio';
+        $user->mnethostid = $CFG->mnet_localhost_id;
+        $user->secret     = random_string(15);
+        $user->password   = '';
+        $user->confirmed  = 1;  // Set the user to confirmed.
+
+        $user = self::save_user($userinfo, $user);
+
+        return $user;
+    }
+
+    /**
+     * Create a new user & update the profile fields
+     *
+     * @param array $userinfo
+     * @param object $user
+     * @return object
+     */
+    private static function save_user(array $userinfo, object $user): object {
+        // Map supplied issuer user info to Moodle user fields.
+        $userfieldmapping = new \core\oauth2\user_field_mapping();
+        $userfieldlist    = $userfieldmapping->get_internalfields();
+        $hasprofilefield  = false;
+        foreach ($userfieldlist as $field) {
+            if (isset($userinfo[ $field ]) && $userinfo[ $field ]) {
+                $user->$field = $userinfo[ $field ];
+
+                // Check whether the profile fields exist or not.
+                $hasprofilefield = $hasprofilefield || strpos($field, \core_user\fields::PROFILE_FIELD_PREFIX) === 0;
+            }
+        }
+
+        // Create a new user.
+        $user->id = user_create_user($user, false, true);
+
+        // If profile fields exist then save custom profile fields data.
+        if ($hasprofilefield) {
+            profile_save_data($user);
+        }
+
+        return $user;
     }
 }
