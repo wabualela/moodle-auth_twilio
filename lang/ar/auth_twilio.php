e8a7956e5978889f8cd2a081fd61ad6c568daac8
wabualela <wailabualela@gmail.com>
2024-03-20 06:59:20 +0300

    Fix languastic errors

--- lang/ar/auth_twilio.php
+++ lang/ar/auth_twilio.php
@@ -66,7 +66,7 @@ $string['age']                = 'الفئة العمرية';
 
 $string['resendtext']         = 'لم تحصل على رمز التفعيل';
 $string['resendbtn']          = 'إعادة إرسال {$a}';
-$string['invalidverificationcode'] = 'رمز التعريف غير صالح.حاول مرة اخرى.';
+$string['invalidverificationcode'] = 'رمز التعريف غير صحيح.حاول مرة اخرى.';
 
 
 
